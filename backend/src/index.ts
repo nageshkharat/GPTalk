34f5161bd489b9600c416f321212a6019d4e2be3
Nagesh Kharat <130240484+nageshkharat@users.noreply.github.com>
2025-07-04 23:45:13 +0530

    Update index.ts

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,7 +1,7 @@
 import app from "./app.js";
 import { connectToDatabase } from "./db/connection.js";
  
-//connections and listeneres  
+//connections and listeneres   
 const PORT = process.env.PORT || 5000; 
 connectToDatabase()  
   .then(() => { 
